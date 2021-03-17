8148305ec72eb64f02babe4916dea38549c3eb32
sei-ri <sei.ri@10antz.co.jp>
2021-03-17 12:32:04 +0900

    added product

---
+++ product/server/service.go
@@ -0,0 +1,15 @@
+package server
+
+import (
+	"log"
+
+	"github.com/sei-ri/microservice.io/api/v1/services"
+	"github.com/sei-ri/microservice.io/product/ent"
+)
+
+var _ services.ProductServiceServer = &Service{}
+
+type Service struct {
+	Store *ent.Client
+	Log   *log.Logger
+}
